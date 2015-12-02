79cc3dec5d4e8a2b6898b79b0ff2f8251fad964e
adityagampa <adityagampa149@gmail.com>
2015-12-02 18:50:21 +0530

    Submit Commit

--- src/sortedArrayPositionsChange.cpp
+++ src/sortedArrayPositionsChange.cpp
@@ -13,7 +13,37 @@ NOTES:
 
 #include <stdio.h>
 
-void * sortedArrayPositionsChange(int *Arr, int len)
+void * sortedArrayPositionsChange(int *Arr, int n)
 {
-	return NULL;
+	int num1 = -1, num2 = -1, i = 1, flag = 0;
+	if (n <= 1 || Arr == NULL)
+		return NULL;
+
+	for (i = 0; i<n - 1; i++)
+	{
+		if (num1 == -1)
+		{
+			if (Arr[i]>Arr[i + 1])
+			{
+				num1 = i;
+				num2 = i + 1;
+			}
+		}
+		else
+		{
+			if (Arr[i]>Arr[i + 1])
+			{
+				num2 = i + 1;
+				flag = 1;
+				break;
+			}
+		}
+	}
+	if (flag == 1)
+	{
+		Arr[num1] = Arr[num1] + Arr[num2];
+		Arr[num2] = Arr[num1] - Arr[num2];
+		Arr[num1] = Arr[num1] - Arr[num2];
+	}
+	return Arr;
 }
