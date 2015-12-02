283274f212b95d162653021c3ab306fd42bcf8b9
adityagampa <adityagampa149@gmail.com>
2015-12-02 18:19:59 +0530

    Submit Commit

--- src/sortedArrayInsertNumber.cpp
+++ src/sortedArrayInsertNumber.cpp
@@ -13,8 +13,52 @@ NOTES: Use realloc to allocate memory.
 
 #include <stdio.h>
 #include <malloc.h>
+int * sortedArrayInsertNumber(int *a, int n, int num)
+{
+	int i = 0, j = n - 1, temp, t1;
+	if (a == NULL || n<=0)
+		return NULL;
+	a = (int *)realloc(a, (n + 1)*sizeof(int));
+	while (1)
+	{
+		if (a[i]>a[i + 1])
+			return NULL;
+		if (a[i] == num || a[j] == num)
+			return NULL;
+		if (a[i]<num && a[j]>num)
+		{
+			i++;
+			j--;
+		}
+		else if (a[i]>num)
+		{
+			temp = num;
+			for (; i <= n; i++)
+			{
+				t1 = a[i];
+				a[i] = temp;
+				temp = t1;
+			}
+			break;
+		}
+		else if (a[j]<num)
+		{
+			temp = num;
+			for (j = j + 1; j <= n; j++)
+			{
+				t1 = a[j];
+				a[j] = temp;
+				temp = t1;
+			}
+			break;
+		}
+	}
+	return a;
+}
 
+/*
 int * sortedArrayInsertNumber(int *Arr, int len, int num)
 {
 	return NULL;
 }
+*/
